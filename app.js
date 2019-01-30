2b90795ae069946bef6d65567b0e290259b688e5
dutraneto <dudutraneto@gmail.com>
2019-01-30 00:17:32 -0500

    fix input text search

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const URL = 'https://www.hatchways.io/api/assessment/students';
 let list = document.getElementById('list');
-let robot = document.getElementById('robot');
-
-function requestFetch() {
-  fetch(URL)
-    .then(response => response.json())
-    .then(myJson => {
-      for (let i of myJson.students) {
-        console.log(i);
-        var ul = document.createElement('ul');
-        var li = document.createElement('li');
-        var grades = [];
-        for (let j = 0; j < i.grades.length; j++) {
-          grades.push(Number(i.grades[j]));
-        }
-        let avg = grades.reduce((acc, grade) => grade + acc) / i.grades.length;
-        list.appendChild(ul);
-        ul.appendChild(li);
-        li.innerHTML = `
+fetch(URL)
+  .then(response => response.json())
+  .then(myJson => {
+    for (let i of myJson.students) {
+      // console.log(i);
+      let ul = document.createElement('ul');
+      let li = document.createElement('li');
+      let grades = [];
+      for (let j = 0; j < i.grades.length; j++) {
+        grades.push(Number(i.grades[j]));
+      }
+      let avg = grades.reduce((acc, grade) => grade + acc) / i.grades.length;
+      list.appendChild(ul);
+      ul.appendChild(li);
+      li.innerHTML = `
                         <div><img class="img" src="${i.pic}"></img></div>
                         <div class="description">
                           <p class="name">${i.firstName} ${i.lastName}</p>
@@ -28,8 +25,22 @@ function requestFetch() {
                         </div>
                         
         `;
-      }
-    });
-}
+    }
+  });
 
-requestFetch();
+function search() {
+  let input = document.getElementById('input');
+  let filter = input.value.toUpperCase(); // I spent almost 2 hours strugling with because thar I forgot parentesis
+  let li = document.getElementsByTagName('li');
+  let name, textValue;
+  for (let l of li) {
+    // console.log(i);
+    name = l.getElementsByTagName('p')[0];
+    console.log(name.textContent);
+    if (name.textContent.toUpperCase().indexOf(filter) > -1) {
+      l.style.display = '';
+    } else {
+      l.style.display = 'none';
+    }
+  }
+}
